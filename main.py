c3e8466bfacbc5589d4aa65188af40e4a966d562
RyndychRD <r.roman1234@yandex.ru>
2019-04-05 11:18:51 +1000

    git

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #TODO unpack dat, find key, unpack di3, in res find studetn, connect to file base, read UID, rename folder, put it on ftp
 import os
 from zipfile import ZipFile
-from   import AES
+from Crypto.Cipher import AES
 
-package_name = 'didpack.dat'
+package_name = 'data/didpack.dat'
 password_zip = b'\xF1\xE0\x31\x44\x6F\xF1\xCF\x32\x47\xF5\x6F\xCD\xC0\xE2\xE0\xF0\x33\xE8'
+print password_zip.decode('UTF 32')
 key_aes = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
 mode = AES.MODE_ECB
 zip_file = ZipFile(package_name)
